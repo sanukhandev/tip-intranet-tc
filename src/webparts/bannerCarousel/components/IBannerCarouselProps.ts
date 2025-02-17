4602398d26615e5f4140d1d2f15b53bc5f6ed1b1
Sanu Khan <Sanu.khan-external@alfuttaim.com>
2025-02-17 13:30:12 +0400

    Banner carousal added

---
+++ src/webparts/bannerCarousel/components/IBannerCarouselProps.ts
@@ -0,0 +1,7 @@
+export interface IBannerCarouselProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/bannerCarousel/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IBannerCarouselWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'BannerCarouselWebPartStrings' {
+  const strings: IBannerCarouselWebPartStrings;
+  export = strings;
+}
