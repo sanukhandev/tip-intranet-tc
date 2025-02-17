1291a5e316cbfd3c711784c873ae1d78c8e4944e
Sanu Khan <Sanu.khan-external@alfuttaim.com>
2025-02-17 16:01:00 +0400

    Add Tabbed List web part with assets, constants, and localization support

--- src/CONSTANTS.ts
+++ src/CONSTANTS.ts
@@ -9,3 +9,7 @@ export const BUY_SELL_ITEMS_LIST = "BuySellItems";
 export const BUY_SELL_ITEMS_IMAGES_LIBRARY = "BuySellItemsImages";
 
 export const EVENTS_LIST_NAME = "Events";
+
+export const ANNOUNCEMENTS_LIST = "Announcements";
+export const EVENTS_LIST = "Announcements";
+export const EMPLOYEE_MASTER = "EmployeeMaster";
